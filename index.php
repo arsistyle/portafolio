9d3743445d124e8e2fddd84fed927e580b268156
Arsi <arsistyle@gmail.com>
2015-08-13 22:42:21 -0300

    Archivos 13/08
    Subida de archivos

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Arsistyle | Portafolio</title>
+	<meta description="Portafolio de proyectos y algo más de Israel Larrondo, alisas Arsistyle">
+
+
+	<!--  Links de los CSSs -->
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,600,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/bootstrap-grid.css">
+
+	<!-- jQuery --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+
+	<script>
+		$(window).scroll(function() {
+		  var scroll = $(window).scrollTop();            
+		  $(".parallax").css("transform","translateY(" +  (scroll)/-10  + "px)");
+		  $(".parallax").css("margin-bottom", ""+ (scroll)/-5 + "px");
+		});
+	</script>
+
+	<script>
+		/*
+
+		Canvas Menú
+
+		*/
+
+		$(function() {
+
+		  var special = ['reveal', 'top', 'boring', 'perspective', 'extra-pop'];
+
+		  // Toggle Nav on Click
+		  $('#menu a').click(function() {
+
+		  	$('.main').toggleClass('main-right');
+		  	$('.menu-icon').toggleClass('icon-top');
+
+		    var transitionClass = $(this).data('transition');
+
+		    if ($.inArray(transitionClass, special) > -1) {
+		      $('body').removeClass();
+		      $('body').addClass(transitionClass);
+		    } else {
+		      $('body').removeClass();
+		      $('#site-canvas').removeClass();
+		      $('#site-canvas').addClass(transitionClass);
+		    }
+
+		    $('#site-wrapper').toggleClass('show-nav');
+
+		    return false;
+
+		  });
+
+		});
+	</script>
+
+	<script>
+		
+	</script>
+
+</head>
+<body>
+	
+	<div id="site-wrapper">
+
+		<div id="site-canvas">
+
+			<div id="site-menu">
+
+				<div class="menu_principal">
+					<div>
+
+					<div class="mini-perfil">
+						<img src="img/me.jpg" alt="yo">
+						<div class="name-job">
+							<span class="name">Arsi Larrondo</span>
+							<span class="job">Diseñador gráfico y web</span>
+						</div>
+					</div>
+						<ul>
+							<!--<object data="img/logo.svg" type="svg"></object>-->
+							<li><a class="current_page" href="#">Inicio
+							<br>
+							<span>Front-page</span></a></li>
+							<li><a href="#">Proyectos</a></li>
+							<li><a href="#">Servicios</a></li>
+							<li><a href="#">Conóceme</a></li>
+							<li><a href="#">Archivos</a></li>
+							<li><a href="#">Contacto</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+
+			<div id="menu" class="menu-icon">
+			<a id="menu-icon" href="#" data-transition="snappy">#</a>
+
+			</div>
+		</div>
+
+	<div class="main">
+
+		<header class="parallax">
+			<div class="logo">O</div>
+			<!-- Mensaje en mobile & tablet version-->
+			<div class="about-front hidden-md hidden-lg">
+			     <div class="about-fron-in">
+			       <div>
+			         <h2>LLáMAME </h2>
+			         <h1>¡Arsistyle!</h1>
+			       </div>
+			     </div>
+			   </div>
+			
+			<!-- Mensaje en desktop version-->
+			<div class="quien-soy head-content hidden-sm hidden-xs col-md-6 col-md-offset-5">
+			<div class="about-front-desktop">
+				<h1>LLáMAME <span>¡Arsistyle!</span></h1>
+			</div>
+				<div class="content-section">
+					<h3>¿Quien soy?</h3>
+					<p>Me llamo Israel Larrondo, pero me conocen como Arsi o Arsistyle en el rubro del Diseño. Soy diseñador gráfico y web, pero me estoy enfocando más en el área del front-end.</p>
+					<a href="#" class="boton">Conóceme</a>
+					<div class="mini-info">Conoce mi perfil completo</div>
+				</div>
+			</div>
+			<div class="icon-down">
+				<a href="#" class="down">V</a>
+			</div>
+		</header>
+
+		<section class="soy z-uno ">
+			<div class="row">
+				<div class="content-section hidden-md hidden-lg col-xs-12 col-md-8 col-md-offset-2">
+					<h3>¿Quien soy?</h3>
+					<p>Me llamo Israel Larrondo, pero me conocen como Arsi o Arsistyle en el rubro del Diseño. Soy diseñador gráfico y web, pero me estoy enfocando más en el área del front-end.</p>
+					<a href="#" class="boton">Conóceme</a>
+					<div class="mini-info">Conoce mi perfil completo</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="content-section hidden-xs hidden-sm col-xs-12 col-md-8 col-md-offset-2">
+					<h3>Habilidades</h3>
+					<p>Me llamo Israel Larrondo, pero me conocen como Arsi o Arsistyle en el rubro del Diseño. Soy diseñador gráfico y web, pero me estoy enfocando más en el área del front-end.</p>
+					<a href="#" class="boton">Conóceme</a>
+					<div class="mini-info">Conoce mi perfil completo</div>
+				</div>
+			</div>
+
+			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" class="separador-top separador-parent" x="0" y="0" viewBox="0 0 960 149.7" style="enable-background:new 0 0 960 149.7;" preserveAspectRatio="none">
+				<path class="separador" d="M960,149.7c-398.5,0-560.5,0-960,0c0-184,0,52,0-132c320.2,48.4,639.8-48.5,960,0
+			C960,201.7,960-34.3,960,149.7z"/>
+			</svg>
+
+			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" class="separador-bottom separador-parent" x="0" y="0" viewBox="0 0 960 149.7" enable-background="new 0 0 960 149.7" preserveAspectRatio="none">
+				<path class="separador" d="M960 8.2c-398.5 0-560.5 0-960 0 0 0 0 50 0 127.5 320.2 48.4 639.8-48.5 960 0C960 56.2 960 8.2 960 8.2z"/>
+			</svg>
+		</section>
+		<section class="proyectos">
+			<div class="row">
+				<div class="content-section col-xs-12 col-md-8 col-md-offset-2">
+					<h3>Mis últimos proyectos</h3>
+				</div>
+			</div>
+		</section>
+	</div>
+
+
+	<footer>
+		
+		<!-- Scrips del sitio -->
+
+	</footer>
+
+	</div><!-- cierre de main-->	
+</body>
+</html>
